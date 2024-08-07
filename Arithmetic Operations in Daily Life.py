88095f106c1ba7f34cb536968948ad49fda81670
fieldboi693 <fieldboi248@gmail.com>
2024-08-07 13:14:58 -0700

    first commit

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,12 @@
+#task 1 
+bread= 10
+peanut_butter= 7
+jelly= 5
+total_cost_of_grocerys = (bread + peanut_butter + jelly)
+print(total_cost_of_grocerys)
+
+#task 2 
+initial_amount = 10000
+yearly_interest = initial_amount * .07
+annual_total = initial_amount + yearly_interest
+print(annual_total)
---
+++ Showcase Your Dance Moves!.py
@@ -0,0 +1,24 @@
+#task 1 
+"correct this code"
+weather = "sunny"
+
+# ")if weather == "sunny":
+# print("Wear sunglasses!")
+# else:
+# print("Take an umbrella!
+
+
+weather = "sunny"
+if weather == "sunny!":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+
+#task 2
+mood = input("what is your mood happy or sad?:")
+print(mood)
+
+if mood == "happy":
+  print("Thats great to hear!")
+else:
+  print("I hope your day gets better!")
